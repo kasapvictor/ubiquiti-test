5316c4e917f205e597471c445d21936a5343cf29
Victor Kasap <kasap.victor@gmail.com>
2023-08-18 16:55:09 -0700

    fix: Fixed serching by shortnames2

--- src/entities/products/model/searchQuery.ts
+++ src/entities/products/model/searchQuery.ts
@@ -1,7 +1,13 @@
 import { createApi, createStore } from 'effector';
 
+import { log } from '@shared/lib';
+
 export const $searchQuery = createStore<string>('');
 
 export const { setQuerySearch } = createApi($searchQuery, {
   setQuerySearch: (_, payload: string) => payload,
+});
+
+$searchQuery.watch((state) => {
+  log('$searchQuery', state);
 });
