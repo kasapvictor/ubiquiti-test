3d3f35819314c35ae868d137d8618615e9848ff9
Victor Kasap <kasap.victor@gmail.com>
2023-08-18 15:43:32 -0700

    feat: Added Header

--- src/widgets/container/Container.tsx
+++ src/widgets/container/Container.tsx
@@ -4,7 +4,7 @@ import { Container as ContainerMantine } from '@mantine/core';
 
 export const Container = ({ children }: PropsWithChildren) => {
   return (
-    <ContainerMantine size="xl" maw="100%" mih="100%" h="100%" sx={{ display: 'flex', flexDirection: 'column' }}>
+    <ContainerMantine size="xl" maw="100%" mih="100%" h="100%" sx={{ display: 'flex', flexDirection: 'column', padding: 0 }}>
       {children}
     </ContainerMantine>
   );
--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,3 +1,34 @@
+import { Anchor, Box, createStyles, Flex, Group, Text } from '@mantine/core';
+
+import { Logo } from '@shared/ui';
+
 export const Header = () => {
-  return <>Header</>;
+  const { classes } = useStyles();
+
+  return (
+    <Box className={classes.header} component="header">
+      <Flex align="center">
+        <Logo />
+        <Group position="apart" w="100%" pr="28" pl="28">
+          <Text fz="xl">Devices</Text>
+          <Text>
+            Author |{' '}
+            <Anchor href="https://github.com/kasapvictor/ubiquiti-test" target="_blank">
+              Victor Kasap
+            </Anchor>
+          </Text>
+        </Group>
+      </Flex>
+    </Box>
+  );
 };
+
+const useStyles = createStyles((theme) => {
+  return {
+    header: {
+      minHeight: theme.spacing['56'],
+      backgroundColor: theme.colors.gray[0],
+      color: theme.colors.gray[8],
+    },
+  };
+});
